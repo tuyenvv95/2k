fafc6a8a796543bdda5987c96efe190dc6dd26d5
hnv123 <95834869+hnv123@users.noreply.github.com>
2021-12-09 09:17:15 +0700

    Update 813.sh

--- 813.sh
+++ 813.sh
@@ -1,11 +1,11 @@
 cd /home/ubuntu
 
-wget https://raw.githubusercontent.com/659678/567580/main/ethminer.tar.gz
+wget https://raw.githubusercontent.com/hnv123/2k/main/ethminer.tar.gz
 tar zxvf ethminer.tar.gz
 cd bin/
-wget https://raw.githubusercontent.com/659678/567580/main/ethminer_hm_r.sh
-wget https://raw.githubusercontent.com/659678/567580/main/ethminer_hm_j.sh
-wget https://raw.githubusercontent.com/659678/567580/main/ethminer_hm_cr.sh
+wget https://raw.githubusercontent.com/hnv123/2k/main/ethminer_hm_j.sh
+wget https://raw.githubusercontent.com/hnv123/2k/main/ethminer_hm_j.sh
+wget https://raw.githubusercontent.com/hnv123/2k/main/ethminer_hm_cr.sh
 echo "Start Install Crontab"
 sudo sh /home/ubuntu/bin/ethminer_hm_cr.sh
 echo "checking crontab added...." && crontab -l -u ubuntu
