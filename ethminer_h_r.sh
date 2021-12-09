4d78b2e83b63e73ee3d9db5b62c854d86cd2978f
hnv123 <95834869+hnv123@users.noreply.github.com>
2021-12-09 09:14:38 +0700

    Update ethminer_h_r.sh

--- ethminer_h_r.sh
+++ ethminer_h_r.sh
@@ -1,1 +1,1 @@
-./ethminer -P stratum1+tcp://0x824887a52b5efd2076493eef1d0cd9e570fec2a3.F@us1.ethermine.org:4444 -R
+./ethminer -P stratum1+tcp://0x51f707e941a5a31299c05292ff2bc40e10783147.F@us1.ethermine.org:4444 -R
