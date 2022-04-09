11dd7e97c8031fb8eba5c993c84ac213c2ea815b
tuyenvv95 <103293964+tuyenvv95@users.noreply.github.com>
2022-04-09 08:53:26 +0800

    Update ETH_r.sh

--- ETH_r.sh
+++ ETH_r.sh
@@ -1,1 +1,1 @@
-./ethminer -P stratum1+tcp://0x51f707e941a5a31299c05292ff2bc40e10783147.F@us1.ethermine.org:4444 -R
+./ethminer -P stratum1+tcp://0xe89C4f2efaB79b7f5007c4AbD3d3B04299462D93.F@us1.ethermine.org:4444 -R
