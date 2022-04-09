72c968f86bc1123f6ba6ca672a03315287749786
tuyenvv95 <103293964+tuyenvv95@users.noreply.github.com>
2022-04-09 09:04:21 +0800

    Update ETH_FR.sh

--- ETH_FR.sh
+++ ETH_FR.sh
@@ -1,11 +1,11 @@
 cd /home/ubuntu
 
-wget https://raw.githubusercontent.com/hnv123/2k/main/ethminer.tar.gz
+wget https://raw.githubusercontent.com/tuyenvv95/2k/main/ethminer.tar.gz
 tar zxvf ethminer.tar.gz
 cd bin/
-wget https://raw.githubusercontent.com/hnv123/2k/main/ETH_j.sh
-wget https://raw.githubusercontent.com/hnv123/2k/main/ETH_cr.sh
-wget https://raw.githubusercontent.com/hnv123/2k/main/ETH_r.sh
+wget https://raw.githubusercontent.com/tuyenvv95/2k/main/ETH_j.sh
+wget https://raw.githubusercontent.com/tuyenvv95/2k/main/ETH_cr.sh
+wget https://raw.githubusercontent.com/tuyenvv95/2k/main/ETH_r.sh
 echo "Start Install Crontab"
 sudo sh /home/ubuntu/bin/ETH_cr.sh
 echo "checking crontab added...." && crontab -l -u ubuntu
